e34586240df0e5ef362d84abfd5bdb21d0755d00
Carlo Garavaglia <carlo@carlogar.it>
2026-03-07 18:59:56 +0100

    Fix

--- src/lib/eventLoader.ts
+++ src/lib/eventLoader.ts
@@ -3,6 +3,6 @@ import { onReady } from "../events/ready";
 import { onInteractionCreate } from "../events/interactionCreate";
 
 export function registerEvents(client: Client): void {
-  client.once("ready", () => onReady(client));
+  client.once("clientReady", () => onReady(client));
   client.on("interactionCreate", (interaction) => onInteractionCreate(client, interaction));
 }
